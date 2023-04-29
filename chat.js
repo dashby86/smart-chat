b6f61cbe1d6d611ff7e2325d5e9e39ee68931e92
Dylan Ashby <dylanashby@Dylans-MBP.lan>
2023-04-29 11:29:48 -0400

    debug

--- chat.js
+++ chat.js
@@ -1,61 +1,10 @@
-function createInputForm() {
-    // Create form elements
-    console.log('Creating input form...');
-    alert("wtf");
-    const form = document.createElement('form');
-    const label = document.createElement('label');
-    const input = document.createElement('input');
-    const button = document.createElement('button');
-
-    // Set attributes and content
-    label.setAttribute('for', 'inputData');
-    label.textContent = 'Enter data:';
-    input.type = 'text';
-    input.id = 'inputData';
-    input.name = 'inputData';
-    button.type = 'submit';
-    button.textContent = 'Submit';
-
-    // Add elements to the form
-    form.appendChild(label);
-    form.appendChild(input);
-    form.appendChild(button);
-
-    // Add form to the DOM
-    document.body.appendChild(form);
-
-    // Attach event listener to the form
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-
-        const data = {
-            value: input.value
-        };
-
-        try {
-            const response = await fetch('https://your-api-endpoint.example.com', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-
-            if (response.ok) {
-                console.log('Data posted successfully:', data);
-            } else {
-                console.error('Error posting data:', response);
-            }
-        } catch (error) {
-            console.error('Error posting data:', error);
-        }
-    });
-    console.log('Created input form...');
-}
-
-// Call the function when the DOM content is loaded
-window.onload = () => {
-    console.log('Page loaded.');
-    createInputForm();
+const HelloWorld = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
 };
 
+const chatWidgetHost = document.querySelector('[data-widget-host="chat-widget"]');
+ReactDOM.render(<HelloWorld />, chatWidgetHost);
