687d72cc5ae80199e8c614098de87eabf7bc1ea2
Dylan Ashby <dylanashby@Dylans-MBP.lan>
2023-04-29 11:54:10 -0400

    debug

--- chat.js
+++ chat.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Self-contained add-on file: addon.js
 
-const HelloWorld = () => {
-    return (
-        <div>
-            Hello World!
-        </div>
-    );
-};
+// Define an IIFE (Immediately Invoked Function Expression) to create a local scope and avoid polluting the global scope
+(function() {
+    // Create a template string with the HTML and CSS for the add-on
+    const addonTemplate = `
+    <style>
+      /* CSS styles for the add-on */
+      #addon-container {
+        font-family: Arial, sans-serif;
+        font-size: 1rem;
+        color: #333;
+      }
+    </style>
+    <div id="addon-container">
+      <!-- HTML elements for the add-on -->
+      <p id="hello-world">Hello World</p>
+    </div>
+  `;
 
-const chatWidgetHost = document.querySelector('[data-widget-host="chat-widget"]');
-ReactDOM.render(<HelloWorld />, chatWidgetHost);
+    // Define a function to initialize the add-on
+    function initAddon() {
+        // Insert the addonTemplate into the DOM
+        const addonContainer = document.createElement('div');
+        addonContainer.innerHTML = addonTemplate;
+        document.body.appendChild(addonContainer);
+
+        // Add any additional JavaScript functionality here
+    }
+
+    // Initialize the add-on when the DOM is fully loaded
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', initAddon);
+    } else {
+        initAddon();
+    }
+})();
