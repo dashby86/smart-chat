05d0641cd1e81e60809434b09c64c0c93f5614e3
dashby <dylan.ashby@rebuyengine.com>
2023-05-11 12:53:08 -0400

    trying to center input field.

--- chat.js
+++ chat.js
@@ -73,6 +73,8 @@
       color: #333;
       display: flex;
       flex-direction: column;
+       justify-content: center;
+  align-items: center;
       width: 100%;
       height: 100%;
       box-sizing: border-box;
